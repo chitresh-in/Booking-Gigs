c29a3dc5577bcae370af58e39f69fa1c5cdf4961
chitresh-in <chitu1997@gmail.com>
2024-11-14 20:57:53 +0400

    Show only published events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
 
   # GET /events or /events.json
   def index
-    @events = Event.all.includes(:host, :venue, :category)
+    @events = Event.published.includes(:host, :venue, :category)
   end
 
   # GET /events/1 or /events/1.json
