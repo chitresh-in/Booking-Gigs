54c70ae742e99e832ed9f0a9041508691889775c
chitresh-in <chitu1997@gmail.com>
2024-11-15 01:20:56 +0400

    Cache venue and category dropdown

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,18 @@ class Category < ApplicationRecord
     acts_as_paranoid
     
     validates :name, presence: true, uniqueness: true
+
+    after_commit :expire_options_for_select_cache
+
+    def self.options_for_select
+        Rails.cache.fetch("category_options_for_select", expires_in: 30.days) do
+            Category.order(:name).map { |category| [category.name, category.id] }
+        end
+    end
+
+    private
+
+    def expire_options_for_select_cache
+        Rails.cache.delete("category_options_for_select")
+    end
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,18 @@
 class Venue < ApplicationRecord
     has_paper_trail
     acts_as_paranoid
+
+    after_commit :expire_options_for_select_cache
+
+    def self.options_for_select
+        Rails.cache.fetch("venue_options_for_select", expires_in: 30.days) do
+            Venue.order(:name).map { |venue| [venue.name, venue.id] }
+        end
+    end
+
+    private
+
+    def expire_options_for_select_cache
+        Rails.cache.delete("venue_options_for_select")
+    end
 end
