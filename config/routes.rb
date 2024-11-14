7b1e666c731072e756794a87f6db95f73af21224
chitresh-in <chitu1997@gmail.com>
2024-11-14 23:40:07 +0400

    Mount sidekiq web UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web => "/sidekiq" #TODO: Add authentication
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
