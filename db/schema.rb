40421c28e72c2a9a6a0efb0631289e6488d6e307
chitresh-in <chitu1997@gmail.com>
2024-11-15 12:22:31 +0400

    Commit schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_14_113949) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_14_155408) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
+
+  create_table "action_text_rich_texts", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "body"
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+  end
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -51,6 +61,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_14_113949) do
     t.datetime "confirmed_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.datetime "requested_at"
     t.index ["event_id"], name: "index_bookings_on_event_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
@@ -92,6 +103,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_14_113949) do
     t.datetime "alloted_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
     t.index ["booking_id"], name: "index_tickets_on_booking_id"
     t.index ["event_id"], name: "index_tickets_on_event_id"
   end
