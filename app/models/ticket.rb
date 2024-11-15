4dd4bb873fe34dab5c0c81489ed1699f1dca80ce
chitresh-in <chitu1997@gmail.com>
2024-11-16 00:24:12 +0400

    Update Ticket.pending_for_allocation to include booking ids from currently running jobs

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -8,8 +8,17 @@ class Ticket < ApplicationRecord
   scope :available_for_booking, -> { where(saleable: true, booking_id: nil) }
 
   def self.pending_for_allocation(event)
-    pending_ticket_allocation_jobs = Sidekiq::Queue.new("ticket_allocation").select { |job| job.klass == "TicketAllocationJob" }
-    booking_ids = pending_ticket_allocation_jobs.map { |job| job.args }.map { |args| args.first }
-    Booking.where(id: booking_ids, event: event).pending.sum(:tickets_count)
+    # Get all booking ids from the queue
+    ticket_allocation_jobs_in_queue = Sidekiq::Queue.new("ticket_allocation").select { |job| job.klass == "TicketAllocationJob" }
+    booking_ids = ticket_allocation_jobs_in_queue.map { |job| job.args }.map { |args| args.first }
+
+    # Get all booking ids from currently running jobs
+    workers = Sidekiq::Workers.new
+    workers.each do |process_id, thread_id, work|
+      if work['queue'] == 'ticket_allocation' && JSON.parse(work['payload'])['class'] == 'TicketAllocationJob'
+        booking_ids << JSON.parse(work['payload'])['args'].first
+      end
+    end
+    Booking.where(id: booking_ids.uniq, event: event).pending.sum(:tickets_count)
   end
 end
