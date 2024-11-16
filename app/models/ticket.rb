465b0e3a85a19bad861f8d015b59b7e0d593a133
chitresh-in <chitu1997@gmail.com>
2024-11-16 17:18:29 +0400

    Update logic for Ticket.in_queue_for_allocation_count

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -7,18 +7,7 @@ class Ticket < ApplicationRecord
 
   scope :available_for_booking, -> { where(saleable: true, booking_id: nil) }
 
-  def self.pending_for_allocation(event)
-    # Get all booking ids from the queue
-    ticket_allocation_jobs_in_queue = Sidekiq::Queue.new("ticket_allocation").select { |job| job.klass == "TicketAllocationJob" }
-    booking_ids = ticket_allocation_jobs_in_queue.map { |job| job.args }.map { |args| args.first }
-
-    # Get all booking ids from currently running jobs
-    workers = Sidekiq::Workers.new
-    workers.each do |process_id, thread_id, work|
-      if work['queue'] == 'ticket_allocation' && JSON.parse(work['payload'])['class'] == 'TicketAllocationJob'
-        booking_ids << JSON.parse(work['payload'])['args'].first
-      end
-    end
-    Booking.where(id: booking_ids.uniq, event: event).pending.sum(:tickets_count)
+  def self.in_queue_for_allocation_count(event)
+    Rails.cache.read(event.tickets_on_hold_count_cache_key, raw: true).to_i
   end
 end
