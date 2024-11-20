ccea80baf4592836b5b0d2f255fba64d0f6e9ddc
chitresh-in <chitu1997@gmail.com>
2024-11-20 23:25:19 +0400

    Handle race condition in TicketAllocationJob

--- app/jobs/ticket_allocation_job.rb
+++ app/jobs/ticket_allocation_job.rb
@@ -3,22 +3,19 @@ class TicketAllocationJob
   sidekiq_options queue: "ticket_allocation"
 
   def perform(booking_id)
-    booking = Booking.find_by(id: booking_id)
+    booking = Booking.lock.find_by(id: booking_id)
     return if booking.nil?
 
     event = booking.event
     return if event.nil?
 
     ActiveRecord::Base.transaction do
-      available_tickets = event.tickets
-        .available_for_booking
-        .lock("FOR UPDATE SKIP LOCKED")
-        .limit(booking.tickets_count)
+      available_ticket_ids = event.tickets.available_for_booking.lock("FOR UPDATE SKIP LOCKED").limit(booking.tickets_count).ids
 
-      if available_tickets.count < booking.tickets_count
+      if available_ticket_ids.length < booking.tickets_count
         booking.update(status: :failed)
       else
-        available_tickets.update_all(booking_id: booking.id)
+        Ticket.where(id: available_ticket_ids).update_all(booking_id: booking.id)
         booking.update(status: :confirmed, confirmed_at: Time.current)
       end
       Rails.cache.decrement(event.tickets_on_hold_count_cache_key, booking.tickets_count)
